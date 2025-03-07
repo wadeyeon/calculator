21ffda5566e57f020b76929c45e31c9c3cc3470e
wadeyeon <wadeyeon@gmail.com>
2025-03-07 10:36:20 +0900

    Create components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import Screen from './components/Screen';
+import Button from './components/Button';
+
+const buttonValues = [
+  ['A/C', '%', '/'],
+  [7, 8, 9, 'X'],
+  [4, 5, 6, '-'],
+  [1, 2, 3, '+'],
+  [0, '.', '='],
+];
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <Screen value={0} />
+        <div className='buttons'>
+          {buttonValues.flat().map((button, index) => {
+            return (
+              <Button
+                key={index}
+                value={button}
+                onClick={() => console.log(`${button} clicked!!`)}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Screen.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Screen({ value }) {
+  return <div>{value}</div>;
+}
